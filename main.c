7b390e5df368682a7b6cfa04c906061a980526b9
Balamorna <gustavo.ramiro14@gmail.com>
2025-02-07 17:01:56 -0300

    Update main.c
    Em tese é pra ser pelo menos uma parte do código feita, mas claramente com um monte de função avulssa

--- main.c
+++ main.c
@@ -1,4 +1,28 @@
-#include <stdio.h>
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void main_routine(AlgoritmoSmo *smo)
+{
+	int NumAlt=0, examT=1;
 
-//TESTINT GITHUB
-Oi
+	while (NumAlt > 0 || examT) {
+		NumAlt = 0;
+
+		if (examT) {
+			for (int i = 0; i < smo->m; i++) {
+				NumAlt += examine_exemplo(smo, i);
+			}
+		}
+		else {
+			NumAlt += primeira_heuristica;
+		}
+
+		if (examT) {
+			examT = 0;
+		}
+		else if (NumAlt == 0) {
+			examT = 1;
+		}
+	}
+}
