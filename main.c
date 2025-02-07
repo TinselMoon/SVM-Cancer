f7667b86ee2534b45b6eb0a6bce9229682f049ed
Balamorna <gustavo.ramiro14@gmail.com>
2025-02-07 19:36:43 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
+struct AlgoritmoSmo 
+{
+	long double** X;
+	long double* y;
+	long double* lagrange;
+	long double* kernel;
+	long double* erro;
+	long double* w;
+	long double tol;
+	long double bias;
+	long double C;
+	long double eps;
+	int dim1, dim2;
+	int otim_lin;
+}AlgoritmoSmo;
+
+int Primeira_Heuristica(AlgoritmoSmo *smo)
+{
+	int NumAlt = 0;
+	int indnlim = getindnlim(smo);
+
+	for (int i = 0; i < indnlim; i++) {
+		NumAlt += examine_exemplo(smo, i);
+	}
+	return NumAlt;
+}
+
 void main_routine(AlgoritmoSmo *smo)
 {
 	int NumAlt=0, examT=1;
@@ -15,7 +43,7 @@ void main_routine(AlgoritmoSmo *smo)
 			}
 		}
 		else {
-			NumAlt += primeira_heuristica;
+			NumAlt += Primeira_Heuristica(smo);
 		}
 
 		if (examT) {
